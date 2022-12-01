646c953c7ca807e0e529b85bb5ff3789ec59e64a
Dinesh <somasekharravada@gmail.com>
2022-12-01 21:55:52 +0530

    Print

---
+++ class2-Average-CipherSchool.py
@@ -0,0 +1,6 @@
+# Ask user to input 3 numbers and you have to print average of three numbers using string formatting
+# Take input in one line
+a,b,c=int(input("Enter 3 number: "))
+
+average=(a+b+c)/3
+print(f"Average of {a},{b},{c},is:",average)
